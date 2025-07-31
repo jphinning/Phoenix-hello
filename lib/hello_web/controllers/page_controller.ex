15d7b59251c1ee70244cadec92df39e2d7fd2756
Joao Hinning <jphinning@gmail.com>
2025-07-31 20:17:45 -0300

    feat: custom routes

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
+  plug HelloWeb.Plugs.Locale, "de" when action in [:home]
+
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
